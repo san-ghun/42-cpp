50f42b02e22d42b9ef4250790327a815ba12dc50
Sanghun <po4865@gmail.com>
2024-02-29 11:02:46 +0100

    feat: Upload ex02 cpp01

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int	main( void )
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "[[ memory address ]]" << "\n";
+	std::cout << "\tof the string variable: " << &str << "\n";
+	std::cout << "\theld by stringPTR: " << stringPTR << "\n";
+	std::cout << "\theld by stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "[[ value ]]" << "\n";
+	std::cout << "\tof the string variable: " << str << "\n";
+	std::cout << "\tpointed to by stringPTR: " << *stringPTR << "\n";
+	std::cout << "\tpointed to by stringREF: " << stringREF << std::endl;
+
+	return (EXIT_SUCCESS);
+}
