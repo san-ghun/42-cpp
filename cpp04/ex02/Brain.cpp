2cc2416d747c276d6063cb135f703de4b6fe6616
Sanghun <po4865@gmail.com>
2024-10-15 23:22:53 +0200

    feat: cpp04/ex02 Abstract Class

---
+++ cpp04/ex02/Brain.cpp
@@ -0,0 +1,46 @@
+#include "Brain.hpp"
+
+Brain::Brain()
+{
+    std::cout << "Brain default constructor called" << std::endl;
+}
+
+Brain::~Brain()
+{
+    std::cout << "Brain destructor called" << std::endl;
+}
+
+Brain::Brain(Brain const &src)
+{
+    *this = src;
+}
+
+Brain &Brain::operator=(Brain const &obj)
+{
+    if (this != &obj)
+    {
+        for (int i = 0; i < 100; i++)
+            this->ideas[i] = obj.ideas[i];
+    }
+    return *this;
+}
+
+std::string	Brain::getIdea(int i) const
+{
+    if (i < 0 || i > 99)
+    {
+        std::cout << "Index out of range" << std::endl;
+        return "";
+    }
+    return this->ideas[i];
+}
+
+void	Brain::setIdea(int i, std::string idea)
+{
+    if (i < 0 || i > 99)
+    {
+        std::cout << "Index out of range" << std::endl;
+        return;
+    }
+    this->ideas[i] = idea;
+}
---
+++ cpp04/ex02/main.cpp
@@ -0,0 +1,63 @@
+#include "AAnimal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "Brain.hpp"
+
+int main()
+{
+    // const AAnimal a;
+    // const AAnimal* b = new AAnimal();
+
+    const AAnimal* i = new Dog();
+    const AAnimal* j = new Cat();
+
+    std::cout << std::endl;
+
+    delete i;
+    delete j;
+    
+    std::cout << "\n----Array testing----" << std::endl;
+
+    AAnimal* AAnimals[4] = { new Dog(), new Cat(), new Dog(), new Cat() };
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (AAnimals[i]->getType() == "Dog")
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                ((Dog*)AAnimals[i])->setIdea(j, "Dog idea");
+            }
+        }
+
+        if (AAnimals[i]->getType() == "Cat")
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                ((Cat*)AAnimals[i])->setIdea(j, "Cat idea");
+            }
+        }
+    }
+    
+    for (int i = 0; i < 4; i++)
+    {
+        delete AAnimals[i];
+    }
+
+    std::cout << "\n----Deep Copy testing----" << std::endl;
+
+    Cat cat;
+    std::cout << &cat << std::endl;
+    cat.setIdea(0, "Cat idea");
+    std::cout << &(cat._brain->ideas[0]) << std::endl;
+    std::cout << cat.getIdea(0) << std::endl;
+
+    std::cout << std::endl;
+
+    Cat cat2 = cat;
+    std::cout << &cat2 << std::endl;
+    std::cout << &(cat2._brain->ideas[0]) << std::endl;
+    std::cout << cat2.getIdea(0) << std::endl;
+
+    return 0;
+}
